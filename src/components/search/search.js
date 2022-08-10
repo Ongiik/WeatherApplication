df69a66343015543fde021441124e101d51c1c3d
Benen Kim <benen.k18@gmail.com>
2022-08-10 15:46:23 -0700

    Created weather widget

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,6 @@
 //component that creates the search bar
 import { useState } from "react";
-import { AsyncPaginate } from "reach-select-async-paginate";
+import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from '../../api';
 const Search = (onSearchChange) => {
 
@@ -8,11 +8,20 @@ const Search = (onSearchChange) => {
 
    const loadOptions = (inputValue) => {
       return fetch(
-         `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
+         `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${inputValue}`, 
          geoApiOptions
       )
-         .then(response => response.json())
-         .then(response => console.log(response))
+         .then((response) => response.json())
+         .then((response) => {
+            return {
+               options: response.data.map((city) => {
+                  return {
+                     value: `${city.latitude} ${city.longitude}`,
+                     label: `${city.name}, ${city.countryCode}`,
+                  }
+               })
+            }
+         })
          .catch(err => console.error(err));
    }
 
