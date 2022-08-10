83547f9df51bd3ec25084826b93e124129373e05
Benen Kim <benen.k18@gmail.com>
2022-08-10 13:26:03 -0700

    first commit, added searchbar and API

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c9da461942msh8f9d653cbbe41bdp1b33a3jsnd3017e3cc65a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
